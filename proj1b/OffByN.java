4987937e91f5ca1f81f491469ca58ea3300732f4
Willen <a528615586@gmail.com>
2022-01-26 11:14:05 +0800

    OffByN: return x == y + n instead of x == y + 5

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -6,7 +6,6 @@ public class OffByN implements CharacterComparator {
     }
     @Override
     public boolean equalChars(char x, char y) {
-        int diff = x - y;
-        return diff == 5 || diff == -5;
+        return x == y + n || y == x + n;
     }
 }
